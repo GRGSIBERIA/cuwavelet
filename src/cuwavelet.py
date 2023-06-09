f51a5433165759238064fdd4e8d563bb7788770f
Eiichi Takebuchi <nanashi4129@gmail.com>
2023-06-09 16:43:22 +0900

    add simple files

---
+++ src/cuwavelet.py
@@ -0,0 +1,14 @@
+import pycuda.autoinit
+import pycuda.driver as driver
+import numpy as np
+from typing import List
+
+class PyWavelet:
+    def __init__(self, intervals: np.array|List) -> None:
+        pass
+
+
+
+
+if __name__ == "__main__":
+    pass
---
+++ src/wavelet.cu
@@ -0,0 +1,3 @@
+__global__ void wavelet_transform() {
+
+}
