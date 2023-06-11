556e122480a4374f1a20bf800936f5ba7fd523ab
Eiichi Takebuchi <nanashi4129@gmail.com>
2023-06-11 16:06:12 +0900

    waveletの長さを調整する
    * waveletの長さによって計算を打ち切る
    * waveletが左端に来るように0を詰める

--- src/cuwavelet.py
+++ src/cuwavelet.py
@@ -6,34 +6,47 @@ from scipy.signal import morlet
 import matplotlib.pyplot as plt
 
 class PyWavelet:
-    def __init__(self, window_length: int, intervals: List) -> None:
+    def __init__(self, window_length: int, sampling_rate: int, intervals: List) -> None:
         self.__waveletsRe = []
         self.__waveletsIm = []
+        self.__wavelet_length = []
         self.__window_length = window_length
+        self.__fs = sampling_rate
 
         for interval in intervals:
-            wavelet = morlet(window_length, 5, interval)
-            re = [x.real for x in wavelet]
-            im = [x.imag for x in wavelet]
-            
+            length = self.__fs / interval
+
+            wavelet = morlet(length * 4, 2, 1)
+            self.__wavelet_length.append(len(wavelet))
+            zeros = np.zeros(self.__window_length - len(re)).tolist()
+
+            re = [x.real for x in wavelet] + zeros
+            im = [x.imag for x in wavelet] + zeros
+
             self.__waveletsRe.append(re)
             self.__waveletsIm.append(im)
+
+            
 
 
 if __name__ == "__main__":
     plt.figure()
     
     Tf = 1. / 44100
-    for i in [4000, 8000, 12000]:
+    for i in [16000]:
         
         length = 44100. / i
         print(length)
-        x = morlet(44100, 5, i)
-        print(x[0].real, x[0].imag)
-        plt.plot(x, marker="x", label=str(i))
+        x = morlet(int(length) * 4, 2, 1)
+        re = [re.real for re in x]
+        im = [im.imag for im in x]
+        plt.plot(re, marker="x", label=str(i))
+        plt.plot(im, marker="x", label=str(i))
 
-    # f = 2*s*w*r / M
+        
+    # f = 2*s*w*sr / length
     # f = 2*1*5*44100 / 44.1
+    # f = 
     
     
     plt.legend()
--- src/wavelet.cu
+++ src/wavelet.cu
@@ -1,16 +1,18 @@
 /*
     @brief ウェーブレット変換を行う
     @param[out] transform Wavelet変換した結果
-    @param[in] wavelets スケーリングした後のWavelet
+    @param[in] waveletsRe スケーリングした後のWaveletの実部
+    @param[in] waveletsIm スケーリングした後のWaveletの虚部
     @param[in] waveform 変換の対象となる波形
     @param[in] timeN 時刻の長さ
     @param[in] scaleN スケールの数
+    @param[in] waveletLength Waveletの長さ
 */
 __global__ void wavelet_transform(
         float* transform,
         float* waveletsRe, float* waveletsIm,
         float* waveform,
-        int timeN, int scaleN
+        int timeN, int scaleN, int* waveletLength
     ) {
     int t = blockDim.x * blockIdx.y + threadIdx.x;    // 時間軸
     int s = blockDim.y * blockIdx.y + threadIdx.y;    // スケール軸
@@ -19,17 +21,20 @@ __global__ void wavelet_transform(
     // 畳み込み積分, f(t)g(i-t)の形式
     float totalRe = 0;
     float totalIm = 0;
-    for (int i = t; i < timeN; ++i) {
-        totalRe += waveforms[t] * waveletsRe[s * scaleN + i - t];
-        totalIm += waveforms[t] * waveletsIm[s * scaleN + i - t];
+    int si = s * scaleN;
+    for (int i = t; i < timeN, i < waveletLength[s]; ++i) {
+        totalRe += waveforms[t] * waveletsRe[si + i - t];
+        totalIm += waveforms[t] * waveletsIm[si + i - t];
     }
     
     transform[idx] = sqrt(totalRe * totalRe + totalIm * totalIm);
 }
 
 /*
+複素数の積は以下で定義される
 z1 * z2 = (x1*x2-y1*y2)+(x1*y2+x2*y1)i
-このとき、z2の虚部がないy2 = 絶対値を取っているとき、
+このとき、z2の虚部がなく、y2 = 0のとき
+z1 * z2 = (x1*x2)+(x2*y1)i
 z1 * z2 = (R1*R2)+(R2*I1)i
 */
 
