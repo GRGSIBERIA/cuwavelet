f92ed0335c2dd0153a5c0f1672b4e57fabc03b63
Eiichi Takebuchi <nanashi4129@gmail.com>
2023-06-12 17:06:34 +0900

    アロケーションの準備をする

--- src/cuwavelet.py
+++ src/cuwavelet.py
@@ -1,14 +1,16 @@
 import pycuda.autoinit
-import pycuda.driver as driver
+import pycuda.driver as cuda
 import numpy as np
 from typing import List
 from scipy.signal import morlet
 import matplotlib.pyplot as plt
+from pycuda.compiler import SourceModule
 
 class PyWavelet:
     def __init__(self, window_length: int, sampling_rate: int, intervals: List) -> None:
         self.__waveletsRe = []
         self.__waveletsIm = []
+        self.__wavelet_scales = len(intervals)
         self.__wavelet_length = []
         self.__window_length = window_length
         self.__fs = sampling_rate
@@ -25,6 +27,34 @@ class PyWavelet:
 
             self.__waveletsRe.append(re)
             self.__waveletsIm.append(im)
+        
+        self.__waveletsRe = np.array(self.__waveletsRe, dtype=np.float32)
+        self.__waveletsIm = np.array(self.__waveletsIm, dtype=np.float32)
+        self.__resultRe = np.zeros(self.__window_length, dtype=np.float32)
+        self.__resultIm = np.zeros(self.__window_length, dtype=np.float32)
+
+        with open("./src/wavelet.cu", "rt") as f:
+            s = f.read()
+            self.__module = SourceModule(s)
+        
+        self.__func = self.__module.get_function("wavelet_transform")
+
+        self.__regpu = cuda.mem_alloc(self.__waveletsRe.nbytes)
+        self.__imgpu = cuda.mem_alloc(self.__waveletsIm.nbytes)
+        self.__resim = cuda.mem_alloc(self.__resultRe.nbytes)
+        self.__resre = cuda.mem_alloc(self.__resultIm.nbytes)
+
+        cuda.memcpy_htod(self.__regpu, self.__waveletsRe)
+        cuda.memcpy_htod(self.__imgpu, self.__waveletsIm)
+    
+    def __del__(self):
+        self.__regpu.free()
+        self.__imgpu.free()
+        self.__resim.free()
+        self.__resre.free()
+    
+    def compute(self, compute_waveform):
+        pass
 
             
 
